1ace3ffebf85109b25229ede3741604f9f8abfb1
ranjan-alt <ranjankumarsah142@gmail.com>
2025-12-03 22:25:43 +0530

    initial fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
 const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI, "mongouri");
 
 const connectDatabase = () => {
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("Mongoose Connected");
-        });
-}
+  mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Mongoose Connected");
+    });
+};
 
 module.exports = connectDatabase;
